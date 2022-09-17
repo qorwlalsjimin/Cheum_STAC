1de02d57e6fc5cb6062b90bc116948099d33259f
ImJaeYeeon <w2109@e-mirim.hs.kr>
2022-09-17 20:32:33 +0900

    java,xml all commit

--- app/src/main/java/com/mirim/cheum_stac/MainActivity.java
+++ app/src/main/java/com/mirim/cheum_stac/MainActivity.java
@@ -6,41 +6,108 @@ import android.content.pm.Signature;
 import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements FragmentListener {
+    private FragmentManager fragmentManager = getSupportFragmentManager();
+    private HomeFragment homeFragment = new HomeFragment();
+    private MapFragment mapFragment = new MapFragment();
+    private FillFragment fillFragment = new FillFragment();
+    private MypageFragment mypageFragment = new MypageFragment();
+
+    //지도 검색 Fragment들
+    private ParentFragment parentFragment;
+    private ChildSearchFragment childSearchFragment;
+    private ChildResultFragment childResultFragment;
+
+    FragmentTransaction transaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getHashKey();
+        transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.frameLayout,homeFragment).commit(); //초기화면 HomeFragment로 지정
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.btn_navi_menu);
+        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener(){
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+                switch (item.getItemId()){
+                    case R.id.tab_home:
+                        transaction.replace(R.id.frameLayout,homeFragment).commit();
+                        break;
+                    case R.id.tab_map:
+                        transaction.replace(R.id.frameLayout,mapFragment).commit();
+                        break;
+                    case R.id.tab_fill:
+                        transaction.replace(R.id.frameLayout,fillFragment).commit();
+                        break;
+                    case R.id.tab_myPage:
+                        transaction.replace(R.id.frameLayout,mypageFragment).commit();
+                        break;
+
+                }
+                return false;
+            }
+        });
+        //지도 검색 Fragment들 정의
+        parentFragment = new ParentFragment();
+        childSearchFragment = new ChildSearchFragment();
+        childResultFragment = new ChildResultFragment();
+
+        gethash(); //키해시 값 구하는 메서드
+
     }
 
+    public void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.frameLayout, fragment).commit();
+    }
 
-    private void getHashKey(){
-        PackageInfo packageInfo = null;
-        try {
-            packageInfo = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
+    //FragmentListener 추상메서드 구현
+    @Override
+    public void onCommand(int index, String data) {
+        switch (index){
+            case 0: //ParentFragment =>
+                childSearchFragment.displayMessage(data);
+                break;
+            case 1: //ChildFavorFragment, ChildSearchFragment =>
+                childResultFragment.displayMessage(data);
+                Log.d("값 옮기기를 추적하자 -_-", "2 Favor에서 받은 값으로 displayMessage를 호출하셧나요? data: "+data);
+                break;
         }
-        if (packageInfo == null)
-            Log.e("KeyHash", "KeyHash:null");
+    }
 
-        for (Signature signature : packageInfo.signatures) {
-            try {
-                MessageDigest md = MessageDigest.getInstance("SHA");
+    //카카오맵 api를 사용하기 위한 키해시 값 구하는 메서드
+    private void gethash() {
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md;
+                md = MessageDigest.getInstance("SHA");
                 md.update(signature.toByteArray());
-                Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT));
-            } catch (NoSuchAlgorithmException e) {
-                Log.e("KeyHash", "Unable to get MessageDigest. signature=" + signature, e);
+                String something = new String(Base64.encode(md.digest(), 0));
+                Log.e("Hash key", something);
+                UserUtils.setHash(something.replaceAll("\n",""));
             }
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            Log.e("name not found", e.toString());
         }
     }
 }
