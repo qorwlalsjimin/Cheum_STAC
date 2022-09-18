0bffca97d6e405d87f6603a9f52f1be277e60ee7
지민 백 <qorwlalsjimin@naver.com>
2022-09-19 03:02:27 +0900

    FavorList에 있는 목록 listview에 출력 (중복 문제 있음)

---
+++ app/src/main/java/com/mirim/cheum_stac/Map/FavorList.java
@@ -0,0 +1,8 @@
+package com.mirim.cheum_stac.Map;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FavorList {
+    public static List<Integer> favorList = new ArrayList<>();
+}
