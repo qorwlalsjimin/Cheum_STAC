d75280a1be8419c9db32425792abcfdcd6dcc57d
ImJaeYeeon <w2109@e-mirim.hs.kr>
2022-09-20 23:10:17 +0900

    news class

--- app/src/main/java/com/mirim/cheum_stac/News.java
+++ app/src/main/java/com/mirim/cheum_stac/News.java
@@ -1,5 +1,15 @@
 package com.mirim.cheum_stac;
 
 public class News {
+    public String title;
+    public String days;
+    public int news;
 
+    public News(){};
+
+    public News(String title, String days,int news){
+        this.title = title;
+        this.days = days;
+        this.news = news;
+    }
 }
