fb77904e6bb2a9717122af26110727433ad5b1d7
juwan-25 <s2109@e-mirim.hs.kr>
2022-09-21 12:17:26 +0900

    좋아요 오류 임시 해결, 뉴스 리사이클러 연결

--- app/src/main/java/com/mirim/cheum_stac/fill_product.java
+++ app/src/main/java/com/mirim/cheum_stac/fill_product.java
@@ -1,11 +1,10 @@
-package com.mirim.cheum_stac.Product.Fragment;
+package com.mirim.cheum_stac;
 
 import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
@@ -15,11 +14,8 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.ValueEventListener;
-import com.mirim.cheum_stac.MainActivity;
 import com.mirim.cheum_stac.Product.HeartList;
 import com.mirim.cheum_stac.Product.ProductList;
-import com.mirim.cheum_stac.R;
-import com.mirim.cheum_stac.fill_detail;
 import com.mirim.cheum_stac.util.FirebaseUtils;
 import com.mirim.cheum_stac.util.UserUtils;
 
@@ -28,6 +24,8 @@ import java.util.List;
 
 public class fill_product extends Fragment {
     MainActivity activity;
+    ImageButton imgbtnHeart;
+    static int productId=7;
 
     @Override
     public void onAttach(@NonNull Context context) {
@@ -53,8 +51,7 @@ public class fill_product extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
-    ImageButton imgbtnHeart;
-    static int productId=7;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -72,7 +69,7 @@ public class fill_product extends Fragment {
             public void onDataChange(DataSnapshot dataSnapshot) {//dataSnapshot : user
                 //즐겨찾기 유무에 따른 boolean값 DB에 insert
                 String path = UserUtils.getHash() + "/heart/" + Integer.toString(productId);
-                Boolean heart = false;
+                boolean heart = false;
                 if (dataSnapshot.child(path).exists()) {
                     heart = dataSnapshot.child(path).getValue(Boolean.class);
                     imgbtnHeart.setBackgroundResource(getBGR(heart));
@@ -80,27 +77,28 @@ public class fill_product extends Fragment {
                 reference.child(path).setValue(heart);
 
                 //즐겨찾기 설정 유무 분별을 위한 setTag
-                if(heart) imgbtnHeart.setTag("heart");
+                if (heart) imgbtnHeart.setTag("heart");
                 else imgbtnHeart.setTag("heart_empty");
 
                 //즐겨찾기한 가게 리스트에 넣기
                 List<Integer> existArr = new ArrayList<>();
-                for(int i = 0; i< ProductList.productList.size(); i++){
-                    String path3 = UserUtils.getHash() + "/heart/"+i;
+                for (int i = 0; i < ProductList.productList.size(); i++) {
+                    String path3 = UserUtils.getHash() + "/heart/" + i;
                     if (dataSnapshot.child(path3).exists()) {
-                        if (dataSnapshot.child(path3).getValue(Boolean.class)){
+                        if (dataSnapshot.child(path3).getValue(Boolean.class)) {
                             existArr.add(Integer.valueOf(i));
                         }
                     }
                 }
-                for(int i = 0; i<existArr.size(); i++){
+                for (int i = 0; i < existArr.size(); i++) {
                     String path2 = UserUtils.getHash() + "/heart/" + existArr.get(i).toString();
-                    if(dataSnapshot.child(path2).getValue(Boolean.class) == Boolean.valueOf(true)){
+                    if (dataSnapshot.child(path2).getValue(Boolean.class) == Boolean.valueOf(true)) {
                         HeartList.heartList[existArr.get(i).intValue()] = 1;
                         //favoriteList 클래스 보고 HeartList 만들면 돼
                         //그 클래스 안에서 hearList까지 만들면 됨
                     }
                 }
+            }
                 @Override
                 public void onCancelled(DatabaseError error) {
                     //에러 처리
@@ -110,19 +108,21 @@ public class fill_product extends Fragment {
         imgbtnHeart.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    Boolean heart = !getBGRHeart(imgbtnHeart.getTag().toString());
+                    boolean heart = !getBGRHeart(imgbtnHeart.getTag().toString());
                     reference.child(path).setValue(heart);
                 }
             });
-        return v;
+
+            return v;
 
         }
 
 
+    public Boolean getBGRHeart(String d) {
+        return d.equals("heart") ? true : false;
+    }
 
-        private Boolean getBGRHeart(String d) {
-            return d.equals("heart") ? true : false;
-        }
-    private int getBGR(Boolean heart) {
+    public int getBGR(Boolean heart) {
         return heart ? R.drawable.heart : R.drawable.heart_empty;
     }
+}
